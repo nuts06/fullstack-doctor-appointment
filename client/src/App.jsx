54293c9a7a036646d56c77c3a822c47cbaad994e
Natasha <121818639+nuts06@users.noreply.github.com>
2025-09-17 10:51:07 +0530

    added the routes and the images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-import './App.css'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Contact from './pages/Contact';
+import Doctor from './pages/About'
+import Login from './pages/Login'
+import About from './pages/About'
 
 function App() {
   return (
-    <>
-      <h1>Hi this is natasha and this is the beginning of the project</h1>
-    </>
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/doctors' element={<Doctor />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/my-appointment' element={<MyAppointments />}/>
+        <Route path='/my-profile' element={<MyProfile />}/>
+      </Routes>
+    </div>
   )
 }
 
